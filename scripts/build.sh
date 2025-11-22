ef9e5d9d14b2dbcb7dcb126086d9d3c4a1d2ee64
ricky <rickycontact9@gmail.com>
2025-11-22 18:59:30 +0100

    don't minify

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,5 +3,5 @@
 rm -rf build
 mkdir build
 cp index.html build
-npx spago bundle-app --source-maps --minify -m Main --to ./build/index.js
-npx spago bundle-app --source-maps --minify -m Worker.Simulation --to ./build/worker.js
+npx spago bundle-app --source-maps -m Main --to ./build/index.js
+npx spago bundle-app --source-maps -m Worker.Simulation --to ./build/worker.js
--- scripts/dev.sh
+++ scripts/dev.sh
@@ -4,7 +4,7 @@ mkdir -p build
 
 npx concurrently --kill-others \
   "nodemon -w index.html -x 'cp index.html build'" \
-  "nodemon -w src -e purs,js -x 'npx spago bundle-app --source-maps --minify -m Main --to ./build/index.js'" \
-  "nodemon -w src -e purs -x 'npx spago bundle-app --source-maps --minify -m Worker.Simulation --to ./build/worker.js'" \
+  "nodemon -w src -e purs,js -x 'npx spago bundle-app --source-maps -m Main --to ./build/index.js'" \
+  "nodemon -w src -e purs -x 'npx spago bundle-app --source-maps -m Worker.Simulation --to ./build/worker.js'" \
   "live-server --middleware=\"$(pwd)/headers-middleware.js\" build"
 
